e2bd73d3c4220d2aec8e841f0b273728299fc745
Autumn Charles <autumn.m.s.c@gmail.com>
2024-10-27 11:49:01 -0400

    completed exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
 	let squares = document.querySelectorAll('#board div');
-	
+	//console.log(squares);
 	let gameState = [];
+	//let gameState = Array(9).fill(null);
+	//let currentPlayer = 'X';
 	let xIsNext = true;
-	
 	squares.forEach((square, index) => {
+		//console.log(squares);
 		square.setAttribute('class','square');
-	
 		gameState[index] = '';
+		
+		square.addEventListener('mouseover', function(){
+			//square.setAttribute('class','square hover');
+			if(gameState[index] == 'X'){
+				square.setAttribute('class','square X hover');
+			} else if(gameState[index] == 'O'){
+				square.setAttribute('class','square O hover');
+			} else {
+				square.setAttribute('class','square hover');
+			}
+		});
+		square.addEventListener('mouseout', function(){
+			//square.setAttribute('class','square');
+			if(gameState[index] == 'X'){
+				square.setAttribute('class','square X');
+			} else if(gameState[index] == 'O'){
+				square.setAttribute('class','square O');
+			} else {
+				square.setAttribute('class','square');
+			}
+			
+		});
+		
 		square.addEventListener('click', function(){
 			if(gameState[index] == ''){
 				let currentPlayer = xIsNext ? 'X' : 'O';
 				square.textContent = currentPlayer;
-				//square.setAttribute('class','square ${currentPlayer}');
 				square.setAttribute('class','square '+currentPlayer);
 				gameState[index] = currentPlayer;
 				xIsNext = !xIsNext;
