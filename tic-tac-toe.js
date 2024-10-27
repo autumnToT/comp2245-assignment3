00fa2c18054cdcdf76328c336338014fc10fcf36
Autumn Charles <autumn.m.s.c@gmail.com>
2024-10-27 10:23:37 -0400

    completed exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(){
-	let squares = document.querySelectorAll('#board div')
-	console.log(squares);
-	squares.forEach((square) => {
-		console.log(square);
+document.addEventListener('DOMContentLoaded', function(){
+	let squares = document.querySelectorAll('#board div');
+	
+	let gameState = [];
+	let xIsNext = true;
+	
+	squares.forEach((square, index) => {
 		square.setAttribute('class','square');
+	
+		gameState[index] = '';
+		square.addEventListener('click', function(){
+			if(gameState[index] == ''){
+				let currentPlayer = xIsNext ? 'X' : 'O';
+				square.textContent = currentPlayer;
+				square.setAttribute('class','square ${currentPlayer}');
+				gameState[index] = currentPlayer;
+				xIsNext = !xIsNext;
+			}
+		});
 	});
 });
